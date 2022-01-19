4592e597a1482510e9f017390cf650b954861ca7
Sael0501 <saelaceves117@gmail.com>
2022-01-19 16:05:31 -0600

    Registro

--- app/src/main/java/mx/kodemia/desafio2/Inicio.kt
+++ app/src/main/java/mx/kodemia/desafio2/Inicio.kt
@@ -1,11 +1,40 @@
 package mx.kodemia.desafio2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_inicio.*
+import android.text.Editable
+import android.text.TextWatcher
 
 class Inicio : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inicio)
+
+        tietPassword.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun afterTextChanged(editPass: Editable?) {
+                if (editPass.toString().trim().isEmpty()) {
+                    tilPassword.setError("Password Required")
+                } else{
+                    tilPassword.setErrorEnabled(false)
+                    tilPassword.setError("")
+                }
+            }
+        })
+
+        buttonLogin.setOnClickListener{
+            startActivity(Intent(this, Registro::class.java))
+        }
+
+        textView2.setOnClickListener{
+            startActivity(Intent(this, Registro::class.java))
+        }
     }
 }
--- app/src/main/java/mx/kodemia/desafio2/Registro.kt
+++ app/src/main/java/mx/kodemia/desafio2/Registro.kt
@@ -1,11 +1,87 @@
 package mx.kodemia.desafio2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import kotlinx.android.synthetic.main.activity_registro.*
 
 class Registro : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registro)
+
+        textView6.setOnClickListener{
+            startActivity(Intent(this,Inicio::class.java))
+        }
+        buttonR.setOnClickListener{
+            startActivity(Intent(this,PantallaPrincipal::class.java))
+        }
+
+        tietUsuarioR.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+            override fun afterTextChanged(editUser: Editable?) {
+                if(editUser.toString().trim().isEmpty()){
+                    tilUsuarioR.setError("Ingrese un usuario")
+                }else{
+                    tilUsuarioR.setErrorEnabled(false)
+                    tilUsuarioR.setError("")
+                }
+            }
+        })
+
+        tietCorreoR.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+            override fun afterTextChanged(editUser: Editable?) {
+                if(editUser.toString().trim().isEmpty()){
+                    tilCorreoR.setError("Ingrese un Email")
+                }else{
+                    tilCorreoR.setErrorEnabled(false)
+                    tilCorreoR.setError("")
+                }
+            }
+        })
+
+        tietPasswordR.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+            override fun afterTextChanged(editUser: Editable?) {
+                if(editUser.toString().trim().isEmpty()){
+                    tilPasswordR.setError("Ingrese una contrasena")
+                }else{
+                    tilPasswordR.setErrorEnabled(false)
+                    tilPasswordR.setError("")
+                }
+            }
+        })
+
+        tietConPasswordR.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+            override fun afterTextChanged(editUser: Editable?) {
+                if(editUser.toString().trim().isEmpty()){
+                    tilConPasswordR.setError("Confirme su contrasena")
+                }else{
+                    tilConPasswordR.setErrorEnabled(false)
+                    tilConPasswordR.setError("")
+                }
+            }
+        })
+
     }
 }
