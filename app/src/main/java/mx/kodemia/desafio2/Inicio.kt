f06a18541ea7224ca47b8feee4ba655e2e190d25
Sael0501 <saelaceves117@gmail.com>
2022-01-19 13:20:29 -0600

    Pantalla inicio Terminal

---
+++ app/src/main/java/mx/kodemia/desafio2/Inicio.kt
@@ -0,0 +1,11 @@
+package mx.kodemia.desafio2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Inicio : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inicio)
+    }
+}
